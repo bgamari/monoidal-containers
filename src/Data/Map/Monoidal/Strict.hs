a364a3a9870d46822dbeb4dc342f853caca14805
Louis Pan <louis@pan.me>
2016-08-01 11:38:15 +1000

    #12 Unhide constructors and added a selector accessor

--- src/Data/Map/Monoidal/Strict.hs
+++ src/Data/Map/Monoidal/Strict.hs
@@ -45,7 +45,7 @@ import Control.Lens
 import Control.Newtype
 
 -- | A 'Map' with monoidal accumulation
-newtype MonoidalMap k a = MM (M.Map k a)
+newtype MonoidalMap k a = MonoidalMap { getMonoidalMap :: M.Map k a }
     deriving (Show, Read, Functor, Eq, Ord, NFData,
               Foldable, Traversable,
               Data, Typeable)
@@ -53,15 +53,15 @@ newtype MonoidalMap k a = MM (M.Map k a)
 type instance Index (MonoidalMap k a) = k
 type instance IxValue (MonoidalMap k a) = a
 instance Ord k => Ixed (MonoidalMap k a) where
-    ix k f (MM m) = case M.lookup k m of
-      Just v  -> f v <&> \v' -> MM (M.insert k v' m)
-      Nothing -> pure (MM m)
+    ix k f (MonoidalMap m) = case M.lookup k m of
+      Just v  -> f v <&> \v' -> MonoidalMap (M.insert k v' m)
+      Nothing -> pure (MonoidalMap m)
     {-# INLINE ix #-}
 
 instance Ord k => At (MonoidalMap k a) where
-    at k f (MM m) = f mv <&> \r -> case r of
-      Nothing -> maybe (MM m) (const (MM $ M.delete k m)) mv
-      Just v' -> MM $ M.insert k v' m
+    at k f (MonoidalMap m) = f mv <&> \r -> case r of
+      Nothing -> maybe (MonoidalMap m) (const (MonoidalMap $ M.delete k m)) mv
+      Just v' -> MonoidalMap $ M.insert k v' m
       where mv = M.lookup k m
     {-# INLINE at #-}
 
@@ -70,18 +70,18 @@ instance Each (MonoidalMap k a) (MonoidalMap k b) a b
 instance Ord k => FunctorWithIndex k (MonoidalMap k)
 instance Ord k => FoldableWithIndex k (MonoidalMap k)
 instance Ord k => TraversableWithIndex k (MonoidalMap k) where
-    itraverse f (MM m) = fmap MM $ itraverse f m
+    itraverse f (MonoidalMap m) = fmap MonoidalMap $ itraverse f m
     {-# INLINE itraverse #-}
 
 instance Ord k => TraverseMin k (MonoidalMap k) where
-    traverseMin f (MM m) = fmap MM $ traverseMin f m
+    traverseMin f (MonoidalMap m) = fmap MonoidalMap $ traverseMin f m
     {-# INLINE traverseMin #-}
 instance Ord k => TraverseMax k (MonoidalMap k) where
-    traverseMax f (MM m) = fmap MM $ traverseMax f m
+    traverseMax f (MonoidalMap m) = fmap MonoidalMap $ traverseMax f m
     {-# INLINE traverseMax #-}
 
 instance AsEmpty (MonoidalMap k a) where
-    _Empty = nearly (MM M.empty) (M.null . unpack)
+    _Empty = nearly (MonoidalMap M.empty) (M.null . unpack)
     {-# INLINE _Empty #-}
 
 instance Wrapped (MonoidalMap k a) where
@@ -90,21 +90,21 @@ instance Wrapped (MonoidalMap k a) where
     {-# INLINE _Wrapped' #-}
 
 instance (Ord k, Monoid a) => Monoid (MonoidalMap k a) where
-    mempty = MM mempty
+    mempty = MonoidalMap mempty
     {-# INLINE mempty #-}
-    MM a `mappend` MM b = MM $ M.unionWith mappend a b
+    MonoidalMap a `mappend` MonoidalMap b = MonoidalMap $ M.unionWith mappend a b
     {-# INLINE mappend #-}
 
 instance Newtype (MonoidalMap k a) (M.Map k a) where
-    pack = MM
+    pack = MonoidalMap
     {-# INLINE pack #-}
-    unpack (MM a) = a
+    unpack (MonoidalMap a) = a
     {-# INLINE unpack #-}
 
 #if MIN_VERSION_base(4,7,0)
 instance (Ord k, Monoid a) => IsList (MonoidalMap k a) where
     type Item (MonoidalMap k a) = (k, a)
-    fromList = MM . M.fromListWith mappend
+    fromList = MonoidalMap . M.fromListWith mappend
     {-# INLINE fromList #-}
     toList = M.toList . unpack
     {-# INLINE toList #-}
@@ -112,7 +112,7 @@ instance (Ord k, Monoid a) => IsList (MonoidalMap k a) where
 
 -- | /O(1)/. A map with a single element.
 singleton :: Ord k => k -> a -> MonoidalMap k a
-singleton k a = MM $ M.singleton k a
+singleton k a = MonoidalMap $ M.singleton k a
 {-# INLINE singleton #-}
 
 -- | /O(1)/. The number of elements in the map.
@@ -140,7 +140,7 @@ findWithDefault def k = M.findWithDefault def k . unpack
 -- | /O(log n)/. Delete a key and its value from the map. When the key is not
 -- a member of the map, the original map is returned.
 delete :: Ord k => k -> MonoidalMap k a -> MonoidalMap k a
-delete k = _Wrapping' MM %~ M.delete k
+delete k = _Wrapping' MonoidalMap %~ M.delete k
 {-# INLINE delete #-}
 
 -- | /O(n)/. Return all elements of the map and their keys
