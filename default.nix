1194da82b2cc5f2a7962608b7e6e4d8052facca6
Ali Abrar <aliabrar@gmail.com>
2019-08-24 06:19:03 -0400

    default.nix: Update for 0.6

--- default.nix
+++ default.nix
@@ -1,17 +1,15 @@
 { mkDerivation, aeson, base, containers, deepseq, hashable, lens
-, newtype, semialign ? null, semigroups, stdenv, these
+, newtype, semialign, semigroups, stdenv, these
 , unordered-containers
 }:
 mkDerivation {
   pname = "monoidal-containers";
-  version = "0.5.0.1";
+  version = "0.6";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base containers deepseq hashable lens newtype
+    aeson base containers deepseq hashable lens newtype semialign
     semigroups these unordered-containers
-  ] ++ stdenv.lib.optional (semialign == null) [ semialign ];
-  configureFlags =
-    stdenv.lib.optional (semialign == null) [ "-f-split-these" ];
+  ];
   homepage = "http://github.com/bgamari/monoidal-containers";
   description = "Containers with monoidal accumulation";
   license = stdenv.lib.licenses.bsd3;
